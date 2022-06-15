9cba90017432b32a930f3994ba99f377fa449858
SerenityInAllThings <petersonvgama@gmail.com>
2022-06-15 07:11:04 -0300

    Adding nvm load logic to build scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 pm2 start app/bin/index.js --name guanabara_status_be --log /logs/guanaba_status_be.log --watch --time
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 pm2 stop guanabara_status_be
