82052ccd2d2fe1048dc28293e7fd2fdef9e38e05
SerenityInAllThings <petersonvgama@gmail.com>
2022-06-15 07:45:33 -0300

    Trying to start app in ec2

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,6 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-pm2 start app/bin/index.js --name guanabara_status_be --log /logs/guanaba_status_be.log --watch --time
+cd /app
+npm install
+pm2 start npm --name guanabara_status_be --log /logs/guanabara_status_be.log --time -- --prefix /app start
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,4 +3,4 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-pm2 stop guanabara_status_be
+pm2 delete guanabara_status_be
