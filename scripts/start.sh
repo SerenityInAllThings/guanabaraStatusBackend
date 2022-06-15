795363fccb37e046580d5f67e8b8d7bdb5c16eb0
SerenityInAllThings <petersonvgama@gmail.com>
2022-06-15 07:52:14 -0300

    Fixing dependencies installation

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,6 +3,5 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-cd /app
-npm install
+npm install --prefix /app start
 pm2 start npm --name guanabara_status_be --log /logs/guanabara_status_be.log --time -- --prefix /app start
