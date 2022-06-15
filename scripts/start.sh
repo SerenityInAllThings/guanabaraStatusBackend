10435c7fe4eec1c8b4ec7e89f69bbd610a5ba62f
SerenityInAllThings <petersonvgama@gmail.com>
2022-06-15 08:00:49 -0300

    Fixing startup script

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,5 +3,5 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-npm install --prefix /app start
+npm install --prefix /app
 pm2 start npm --name guanabara_status_be --log /logs/guanabara_status_be.log --time -- --prefix /app start
